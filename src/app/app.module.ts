2f9366103db03cf96e087e4e2cc5473c02a5484c
Brenton <brenton.pratt@gmail.com>
2019-03-13 09:28:22 -0600

    added firebase capabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {environment} from "../environments/environment.prod";
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+const firebaseConfig = environment.firebaseConfig;
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+export var config = {
+  production: false,
+  apiKey: "AIzaSyA4zj0vFHbzLZg6tw1IozNC0pBaY0_TxnM",
+  authDomain: "capstone-project-5e5ad.firebaseapp.com",
+  databaseURL: "https://capstone-project-5e5ad.firebaseio.com",
+  projectId: "capstone-project-5e5ad",
+  storageBucket: "capstone-project-5e5ad.appspot.com",
+  messagingSenderId: "1057685577409"};
 
 /*
  * For easier debugging in development mode, you can import the following file
